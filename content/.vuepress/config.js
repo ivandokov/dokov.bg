1ef860028890389fc8d22dd06f56b9c8c89a49d7
Ivan Dokov <ivan@jetspark.io>
2019-05-17 13:50:55 +0300

    Fixed wrong installation of GA plugin

--- content/.vuepress/config.js
+++ content/.vuepress/config.js
@@ -1,15 +1,15 @@
 module.exports = {
     title: 'Ivan Dokov - Web Developer',
     description: 'Web development focused blog.',
-    plugins: {
-        'vuepress-plugin-rss': {
-            base_url: '/', // required
-            site_url: 'http://dokov.bg', // required
-            copyright: 'Ivan Dokov', // optional
-            count: 20 // How much articles
-        },
-        '@vuepress/google-analytics': {
-            ga: 'UA-29469126-4'
-        }
-    }
+    plugins: [
+        ['vuepress-plugin-rss', {
+            base_url: '/',
+            site_url: 'http://dokov.bg',
+            copyright: 'Ivan Dokov',
+            count: 20
+        }],
+        ['@vuepress/plugin-google-analytics', {
+            'ga': 'UA-29469126-4'
+        }]
+    ]
 };
