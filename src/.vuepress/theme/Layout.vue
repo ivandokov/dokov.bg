713d56c893d280dfd08a0b197f6ce4e45d56cec7
Ivan Dokov <ivan@jetspark.io>
2019-11-05 12:06:12 +0200

    Install vuepress locally. This fixes the Google Analytics issue.

--- src/.vuepress/theme/Layout.vue
+++ src/.vuepress/theme/Layout.vue
@@ -31,35 +31,6 @@
             year() {
                 return (new Date()).getFullYear();
             }
-        },
-
-        mounted() {
-            // Temp workaround for Google Analytics until a solution is found for https://github.com/vuejs/vuepress/issues/1608
-            const router = this.$router;
-            const GA_ID = 'UA-29469126-4';
-            if (process.env.NODE_ENV === 'production' && typeof window !== 'undefined') {
-                (function (i, s, o, g, r, a, m) {
-                    i['GoogleAnalyticsObject'] = r;
-                    i[r] = i[r] || function () {
-                        (i[r].q = i[r].q || []).push(arguments);
-                    }
-                    i[r].l = 1 * new Date();
-                    a = s.createElement(o);
-                    m = s.getElementsByTagName(o)[0];
-                    a.async = 1;
-                    a.src = g;
-                    m.parentNode.insertBefore(a, m);
-                })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
-
-                ga('create', GA_ID, 'auto');
-                ga('set', 'anonymizeIp', true);
-                ga('send', 'pageview');
-
-                router.afterEach(function (to) {
-                    ga('set', 'page', to.fullPath);
-                    ga('send', 'pageview');
-                });
-            }
         }
     }
 </script>
