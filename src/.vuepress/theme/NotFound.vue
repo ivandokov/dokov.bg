d43dfc2624a58317e609decc2fcce9f08e96d409
Ivan Dokov <ivan@jetspark.io>
2019-11-11 14:09:42 +0200

    Fixed 404 page

--- src/.vuepress/theme/NotFound.vue
+++ src/.vuepress/theme/NotFound.vue
@@ -1,30 +1,21 @@
 <template>
-    <div class="container">
-        <Sidebar></Sidebar>
-        <main class="main">
-            <h1>{{ getMsg() }}</h1>
-        </main>
+    <div class="pb-12 px-3 md:px-0 font-sans text-secondary">
+        <div class="container flex flex-wrap">
+            <Sidebar class="w-full md:w-1/4 md:text-right"/>
+            <main class="w-full md:w-3/4 md:pl-10 md:pt-24 mt-6 text-lg leading-relaxed">
+                <section class="text-5xl leading-none font-bold mb-2">
+                    <h1>Oh Snap! The page is missing...</h1>
+                </section>
+            </main>
+        </div>
     </div>
 </template>
 
 <script>
     import Sidebar from './components/Sidebar.vue'
 
-    const msgs = [
-        `There's nothing here.`,
-        `How did we get here?`,
-        `That's a Four-Oh-Four.`,
-        `Looks like we've got some broken links.`
-    ];
-
     export default {
         components: { Sidebar },
-
-        methods: {
-            getMsg() {
-                return msgs[Math.floor(Math.random() * msgs.length)]
-            }
-        }
     }
 </script>
 
