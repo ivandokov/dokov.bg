1bc7b906ba4adf164f2e67c7da5b84acaf3f9978
Ivan Dokov <ivan@jetspark.io>
2023-07-10 09:47:45 +0300

    Remove post date from index page

--- src/.vuepress/theme/pages/Index.vue
+++ src/.vuepress/theme/pages/Index.vue
@@ -4,36 +4,33 @@
             <h2 class="text-4xl leading-none font-bold mb-2">
                 <router-link :to="post.path">{{ post.title }}</router-link>
             </h2>
-            <div class="mb-1 text-xs text-gray-500">
-                {{ post.frontmatter.date | date }}
-            </div>
             <div v-html="post.frontmatter.excerpt"></div>
         </section>
     </article>
 </template>
 
 <script>
-    import { SortedPosts } from '../posts'
+import { SortedPosts } from '../posts'
 
-    export default {
-        data() {
-            return {
-                title: null,
+export default {
+    data() {
+        return {
+            title: null,
+        }
+    },
+
+    computed: {
+        items() {
+            let posts = SortedPosts(this.$site.pages);
+
+            if (this.$page.path !== '/') {
+                this.title = this.$page.title;
             }
+
+            return posts.filter(post => {
+                return post.path.match(new RegExp(`^${this.$page.path}`))
+            });
         },
-
-        computed: {
-            items() {
-                let posts = SortedPosts(this.$site.pages);
-
-                if (this.$page.path !== '/') {
-                    this.title = this.$page.title;
-                }
-
-                return posts.filter(post => {
-                    return post.path.match(new RegExp(`^${this.$page.path}`))
-                });
-            },
-        },
-    }
+    },
+}
 </script>
